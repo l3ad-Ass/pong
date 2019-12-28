48a523aea93c105244a4b4f2e3eb42490df0260b
Rishabh Rathore <rrathore9003@gmail.com>
2019-12-28 19:05:49 +0530

    Some refactoring and Improvents and Test setup

--- js/main.js
+++ js/main.js
@@ -11,14 +11,48 @@ function init(e) {
 	g = new Ground(100, '#00f000');
 
 	p1 = new Player(1, g);
-	p2 = new Player(2, g);
+	//p2 = new Player(2, g);
 
-	b = new Ball(g, [ p1, p2 ]);
+	b = new Ball(g, [ p1 ]);
 
 	animateInterval = requestAnimationFrame(animate);
 }
+//Animation loop
+function animate() {
+	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
+	//Drawing calls
+	g.draw();
+	b.draw();
+	p1.draw();
+	//p2.draw();
+	//Update Calls
+	b.update();
+
+	//Recurr animate function
+	requestAnimationFrame(animate);
+}
 //Global functions
+
+//Collision Square and circle
+function intersects(circle,rect)
+{
+	var circleDistance={};
+    circleDistance.x = Math.abs(circle.x - rect.x);
+    circleDistance.y = Math.abs(circle.y - rect.y);
+
+    if (circleDistance.x > (rect.width/2 + circle.radius)) { return false; }
+    if (circleDistance.y > (rect.height/2 + circle.radius)) { return false; }
+
+    if (circleDistance.x <= (rect.width/2)) { return true; } 
+    if (circleDistance.y <= (rect.height/2)) { return true; }
+
+    var cornerDistance_sq = (circleDistance.x - rect.width/2)^2 +
+                         (circleDistance.y - rect.height/2)^2;
+
+    return (cornerDistance_sq <= (circle.radius^2));
+}
+
 
 //Ground class
 function Ground(margin, color) {
@@ -38,20 +72,41 @@ function Ground(margin, color) {
 
 //Ball class
 function Ball(ground, players) {
-	this.x = ground.x + ground.width / 2;
-	this.y = ground.y + ground.height / 2;
+	this.radius = 4;
+	this.x = ground.x - this.radius + ground.width / 2;
+	this.y = ground.y - this.radius + ground.height / 2;
 	this.speed = 3;
 	this.angle = 10;
 	this.color = '#ff0000';
 	this.players = players;
+	//test
+	this.radius=40;
+	this.speed = 0.1;
+	this.x = 140;
+	 this.y=160;
+	this.angle = 1;
+	//
 	this.setAngle = function(angle) {
+		if (angle >= 360) {
+			this.setAngle(angle - 360);
+			return;
+		} else if (angle < 0) {
+			this.setAngle(360 + angle);
+			return;
+		}
 		this.angle = angle;
 	};
 	this.draw = function() {
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
-		ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
+		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
 		ctx.fill();
+		ctx.closePath();
+		//Outline
+		ctx.beginPath();
+		ctx.strokeStyle='blue';
+		ctx.rect(this.x-this.radius,this.y-this.radius,2*this.radius,2*this.radius);
+		ctx.stroke();
 	};
 	this.update = function() {
 		//Update the new coordinates according to speed
@@ -60,28 +115,45 @@ function Ball(ground, players) {
 
 		//Logic for Wall bounce
 		//Right and Left respectively
-		if (this.x + 5 >= ground.margin + ground.width || this.x - 5 <= ground.margin) {
+		if (this.x + this.radius >= ground.margin + ground.width || this.x - this.radius <= ground.margin) {
 			this.setAngle(180 - this.angle);
 		}
 		//Bottom and Top respectively
-		if (this.y + 5 >= ground.margin + ground.height || this.y - 5 <= ground.margin) {
+		if (this.y + this.radius >= ground.margin + ground.height || this.y - this.radius <= ground.margin) {
 			this.setAngle(360 - this.angle);
 		}
 
-        //Logic for player collision
-        var currentBall=this;
-		this.players.forEach(function(player){
-            //Bounce detection logic
-			if (currentBall.x + 5 >= player.x && currentBall.y + 5 >= player.y&&currentBall.x-5<=player.x+player.width&&currentBall.y-5<=player.y+player.height) {
-                
-                if(currentBall.x + 5 >= player.x || currentBall.x-5<=player.x+player.width){
-                    currentBall.setAngle(180 - currentBall.angle);
-                } else if(currentBall.y + 5 >= player.y || currentBall.y-5<=player.y+player.height){
-                    currentBall.setAngle(360 - currentBall.angle);
-                }
-            }
-            
-        });
+		//Logic for player collision
+		for (var i = 0; i < players.length; i++) {
+			let player = players[i];
+			//Bounce from player detection logic
+			// if (
+			// 	this.x - 5 <= player.x + playerMarginW &&
+			// 	this.x + 5 >= player.x - playerMarginW&&
+			// 	this.y - 5 <= player.y + playerMarginW &&
+			// 	this.y + 5 >= player.y-playerMarginW
+			// ) {
+			// 	//console.log(player.number + ') bounce from');
+			// 	let distance=Math.sqrt((player.x-this.x)*(player.x-this.x)+(player.y-this.y)*(player.y-this.y));
+				
+			// 	//console.log(distance);
+			// 	if(distance==){
+			// 		console.log('not center');
+					
+			// 	}else{
+			// 		console.log('center');
+					
+			// 	}
+			// }
+			if(intersects(this,player)){
+				let margin = player.width/2;
+				if(this.x+this.radius>player.x-margin&&this.x+this.radius<player.x+margin){
+					this.setAngle(360-this.angle);
+					console.log('.');
+				}
+
+			}
+		}
 	};
 }
 
@@ -89,13 +161,14 @@ function Ball(ground, players) {
 function Player(number, ground) {
 	this.x;
 	this.y;
-	this.width = 5;
-	this.height = 30;
+	this.width = 50;
+	this.height = 130;
+	this.number = number;
 	this.color;
 	if (number == 1) {
-		this.x = ground.margin + 2;
-		this.y = ground.height / 2 + ground.margin - this.height / 2;
-		this.color = '#f0f0f0';
+		this.x = ground.margin+this.width/2 + 120;
+		this.y = ground.height / 2 + ground.margin+this.height/2 - this.height / 2;
+		this.color = 'rgba(233,233,233,0.5)';//'#f0f0f0';
 	} else if (number == 2) {
 		this.x = ground.margin + ground.width - (2 + this.width);
 		this.y = ground.height / 2 + ground.margin - this.height / 2;
@@ -106,27 +179,13 @@ function Player(number, ground) {
 	this.draw = function() {
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
-		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.rect(this.x-this.width/2, this.y-this.height/2, this.width, this.height);
 		ctx.fill();
 	};
 	this.update = function() {};
 }
 
-//Animation loop
-function animate() {
-	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-	//Drawing calls
-	g.draw();
-	b.draw();
-	p1.draw();
-	p2.draw();
-	//Update Calls
-	b.update();
-
-	//Recurr animate function
-	requestAnimationFrame(animate);
-}
 
 //Resize adjustment
 
