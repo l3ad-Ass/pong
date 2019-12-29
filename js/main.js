e8f154ece1809cfd12f3f8b23ab598b3cd32232f
Rishabh Rathore <rrathore9003@gmail.com>
2019-12-29 13:38:27 +0530

    Added basic stuff and little responsiveness

--- js/main.js
+++ js/main.js
@@ -36,6 +36,11 @@ function init(e) {
 	function resize(e) {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
+		isPaused=true;
+		g.reset();
+		b.reset();
+		p1.reset();
+		p2.reset();
 	}
 }
 //Animation loop
@@ -44,6 +49,8 @@ function animate() {
 
 	//Drawing calls
 	g.draw();
+	p1.drawScore();
+	p2.drawScore();
 	b.draw();
 	p1.draw();
 	p2.draw();
@@ -60,17 +67,18 @@ function animate() {
 
 //Collision Square and circle
 function intersects(circle, rect) {
+	
 	var circleDistance = {};
 	circleDistance.x = Math.abs(circle.x - rect.x);
 	circleDistance.y = Math.abs(circle.y - rect.y);
-
+	
 	if (circleDistance.x > rect.width / 2 + circle.radius) {
 		return false;
 	}
 	if (circleDistance.y > rect.height / 2 + circle.radius) {
 		return false;
 	}
-
+	
 	if (circleDistance.x <= rect.width / 2) {
 		return true;
 	}
@@ -86,25 +94,43 @@ function intersects(circle, rect) {
 //Ground class
 function Ground(margin, color) {
 	this.margin = margin;
-	this.x = margin;
+	this.x = (window.innerWidth-200>=800)?(window.innerWidth-800)/2:margin;
 	this.y = margin;
-	this.width = window.innerWidth - 2 * margin;
+	this.width = (window.innerWidth-200>=800)?800:window.innerWidth - 2 * margin;
 	this.height = window.innerHeight - 2 * margin;
 	this.color = color;
 	this.draw = function() {
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
 		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.stroke();
 		ctx.fill();
+		ctx.font = "100px Comic Sans M";
+		ctx.fillStyle=this.color;
+		ctx.textAlign='center';
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = '#ffffff';
+		for(var i=this.y-100;i<=this.height+100;i+=15){
+			ctx.rect(this.x+this.width/2-2,i+10,4,10);
+		}
+		ctx.fill();
+		ctx.fillText('PONG', this.x+this.width/2,this.margin+this.height-20);
+	};
+	this.reset=function(){
+		this.height = (window.innerHeight<=500)?300: window.innerHeight - 2 * margin;
+		this.x = (window.innerWidth-200>=800)?(window.innerWidth-800)/2:margin;
+		this.width=(window.innerWidth-200>=800)?800:((window.innerWidth<=500)?300:window.innerWidth - 2 * margin);
+
 	};
 }
 
 //Ball class
 function Ball(ground, players) {
-	this.radius = 4;
-	this.x = ground.x - this.radius + ground.width / 2;
-	this.y = ground.y - this.radius + ground.height / 2;
-	this.speed = 3;
+	this.radius = ground.height*0.03;
+	this.x = ground.x + ground.width / 2;
+	this.y = ground.y + ground.height / 2;
+	this.speed = 5;
 	this.velocityX = this.speed;
 	this.velocityY = 0;
 	this.color = '#ff0000';
@@ -122,29 +148,30 @@ function Ball(ground, players) {
 		ctx.fillStyle = this.color;
 		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
 		ctx.fill();
-		ctx.closePath();
-		//Outline
-		ctx.beginPath();
-		ctx.strokeStyle = 'blue';
-		ctx.moveTo(this.x - this.radius, 0);
-		ctx.lineTo(this.x - this.radius, canvas.width);
-		ctx.moveTo(this.x + this.radius, 0);
-		ctx.lineTo(this.x + this.radius, canvas.width);
-		ctx.moveTo(0, this.y - this.radius);
-		ctx.lineTo(canvas.width, this.y - this.radius);
-		ctx.moveTo(0, this.y + this.radius);
-		ctx.lineTo(canvas.width, this.y + this.radius);
 		ctx.stroke();
 		ctx.closePath();
-		//axis
-		ctx.beginPath();
-		ctx.strokeStyle = 'white';
-		ctx.moveTo(this.x, 0);
-		ctx.lineTo(this.x, canvas.height);
-		ctx.moveTo(0, this.y);
-		ctx.lineTo(canvas.width, this.y);
-		ctx.stroke();
-		ctx.closePath();
+		// //Outline
+		// ctx.beginPath();
+		// ctx.strokeStyle = 'blue';
+		// ctx.moveTo(this.x - this.radius, 0);
+		// ctx.lineTo(this.x - this.radius, canvas.width);
+		// ctx.moveTo(this.x + this.radius, 0);
+		// ctx.lineTo(this.x + this.radius, canvas.width);
+		// ctx.moveTo(0, this.y - this.radius);
+		// ctx.lineTo(canvas.width, this.y - this.radius);
+		// ctx.moveTo(0, this.y + this.radius);
+		// ctx.lineTo(canvas.width, this.y + this.radius);
+		// ctx.stroke();
+		// ctx.closePath();
+		// //axis
+		// ctx.beginPath();
+		// ctx.strokeStyle = 'white';
+		// ctx.moveTo(this.x, 0);
+		// ctx.lineTo(this.x, canvas.height);
+		// ctx.moveTo(0, this.y);
+		// ctx.lineTo(canvas.width, this.y);
+		// ctx.stroke();
+		// ctx.closePath();
 	};
 	this.update = function() {
 		//Update the new coordinates according to speed
@@ -153,11 +180,11 @@ function Ball(ground, players) {
 
 		//Logic for Wall bounce
 		//Right and Left respectively
-		if (this.x + this.radius >= ground.margin + ground.width || this.x - this.radius <= ground.margin) {
+		if (this.x + this.radius >= ground.x + ground.width || this.x - this.radius <= ground.x) {
 			this.velocityX *= -1;
 		}
 		//Bottom and Top respectively
-		if (this.y + this.radius >= ground.margin + ground.height || this.y - this.radius <= ground.margin) {
+		if (this.y + this.radius >= ground.y + ground.height || this.y - this.radius <= ground.y) {
 			this.velocityY *= -1;
 		}
 
@@ -175,20 +202,21 @@ function Ball(ground, players) {
 			}
 		}
 		//Point logic
-		if (this.x + this.radius >= ground.margin + ground.width || this.x - this.radius <= ground.margin) {
-			this.resetBall();
+		if (this.x + this.radius >= ground.x + ground.width || this.x - this.radius <= ground.x) {
 			isPaused = true;
-			if (this.x < ground.width / 2) {
+			if (this.x < canvas.width / 2) {
 				players[1].score++;
 			} else {
-				players[1].score++;
+				players[0].score++;
+				
 			}
+			this.reset();
 		}
 	};
-	this.resetBall = function() {
-		this.x = ground.x - this.radius + ground.width / 2;
-		this.y = ground.y - this.radius + ground.height / 2;
-		this.speed = 3;
+	this.reset = function() {
+		this.x = ground.x + ground.width / 2;
+		this.y = ground.y + ground.height / 2;
+		this.speed = 5	;
 		this.velocityX = this.speed;
 		this.velocityY = 0;
 	};
@@ -198,18 +226,18 @@ function Ball(ground, players) {
 function Player(number, ground) {
 	this.x;
 	this.y;
-	this.width = 15;
-	this.height = 70;
+	this.width = ground.height*0.03;
+	this.height = ground.height*0.15;
 	this.speed=3;
 	this.number = number;
 	this.score = 0;
 	this.color;
 	if (number == 1) {
-		this.x = ground.margin + this.width / 2 + 2;
+		this.x = ground.x + this.width / 2 + 2;
 		this.y = ground.height / 2 + ground.margin + this.height / 2 - this.height / 2;
 		this.color = '#f0f0f0';
 	} else if (number == 2) {
-		this.x = ground.margin + ground.width - (2 + this.width / 2);
+		this.x = ground.x + ground.width - (2 + this.width / 2);
 		this.y = ground.height / 2 + ground.margin;
 		this.color = '#0f0f0f';
 	} else {
@@ -223,8 +251,34 @@ function Player(number, ground) {
 	};
 	this.update = function(newY) {
 		
-		if(newY>ground.y+this.height/2&&newY<ground.y+ground.height-this.height/2){	
+		if(newY-this.height/2>ground.y&&newY+this.height/2<ground.y+ground.height){	
 			this.y=newY;
+		}else if(newY-this.height/2<=ground.y){
+			this.y=ground.y+this.height/2;
+		}else if(newY+this.height/2>=ground.y+ground.height){
+			this.y=ground.y+ground.height-this.height/2;
+		}
+	};
+	this.drawScore=function(){
+		ctx.font = "100px Comic Sans M";
+		ctx.fillStyle='#ffffff';
+		ctx.textAlign='center';
+		if(this.number==1){
+			ctx.fillText(this.score,ground.x+ ground.width/4, ground.margin*2);
+		}else{
+			ctx.fillText(this.score, ground.x+3*ground.width/4, ground.margin*2);
+		}
+	};
+	this.reset=function(){
+		this.height = ground.height*0.15;
+		if (this.number == 1) {
+			this.x = ground.x + this.width / 2 + 2;
+			this.y = ground.height / 2 + ground.margin + this.height / 2 - this.height / 2;
+		} else if (this.number == 2) {
+			this.x = ground.x + ground.width - (2 + this.width / 2);
+			this.y = ground.height / 2 + ground.margin;
+		} else {
+			console.error('Player number is not valid : ' + number);
 		}
 	};
 }
